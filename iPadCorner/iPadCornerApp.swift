6f07fb962f57a5ddfb60db8b9f699c45f282294d
Amir Pahadi <apahadi73@gmail.com>
2020-09-23 20:46:35 -0500

    Initial Commit

---
+++ iPadCorner/iPadCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPadCornerApp.swift
+//  iPadCorner
+//
+//  Created by Amir Pahadi on 9/23/20.
+//
+
+import SwiftUI
+
+@main
+struct iPadCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
